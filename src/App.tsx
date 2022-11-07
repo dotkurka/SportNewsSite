560a1fd804e23377de059ec968ef012f99aabb12
dotkurka <moizeromg@gmail.com>
2022-11-07 23:52:11 +0200

    feat: login page in processing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { LogInPage } from './pages';
+
+import './styles/App.scss';
 
 function App() {
-  return <div className='App'>heloo</div>;
+  return (
+    <div className='App'>
+      <LogInPage />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/LogInPage/LogInPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './LogInPage.scss';
+
+function LogInPage() {
+  return (
+    <div className='log-in'>
+      <div className='log-in-left'>
+        <div className='log-in-logo'>
+          <span className='log-in-logo-text'>Sport News</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LogInPage;
