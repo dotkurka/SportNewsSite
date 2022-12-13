9d0a4502d1ecdf583328aa9467bb978769ea0578
dotkurka <moizeromg@gmail.com>
2022-12-14 00:15:57 +0200

    feat: add follow block

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,3 +1,8 @@
+import { ReactComponent as FacebookIcon } from '../../assets/images/facebook-follow-icon.svg';
+import { ReactComponent as GoogleIcon } from '../../assets/images/google-follow-icon.svg';
+import { ReactComponent as TwitterIcon } from '../../assets/images/twitter-follow-icon.svg';
+import { ReactComponent as YoutubeIcon } from '../../assets/images/youtube-follow-icon.svg';
+
 import './SideBar.scss';
 
 const SideBar = () => {
@@ -16,6 +21,19 @@ const SideBar = () => {
         <a href='href' className='sidebar-item'>
           dfdf
         </a>
+        <div className='sidebar-follow'>
+          Follow
+          <div className='sidebar-follow-icon'>
+            <div className='sidebar-follow-icon-contain'>
+              <FacebookIcon className='sidebar-follow-icon-item' />
+              <TwitterIcon className='sidebar-follow-icon-item' />
+            </div>
+            <div className='sidebar-follow-icon-contain'>
+              <GoogleIcon className='sidebar-follow-icon-item' />
+              <YoutubeIcon className='sidebar-follow-icon-item' />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
