1b18114a3540c28936a77348b076a62f54c7f633
dotkurka <moizeromg@gmail.com>
2023-01-19 16:28:55 +0200

    fix: absolute imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-
 import { Logo } from './components';
 import { SidebarData } from './config/SideBarData/SidebarData';
 import SideBar from './layouts/MainLayout/components/SideBar/SideBar';
-
 
 import './styles/App.scss';
 
