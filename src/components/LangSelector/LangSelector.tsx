f112e56e67d0246f1920b828814b02844872f4e2
dotkurka <moizeromg@gmail.com>
2023-01-23 20:52:42 +0200

    feat: create lang selector

--- src/components/LangSelector/LangSelector.tsx
+++ src/components/LangSelector/LangSelector.tsx
@@ -5,26 +5,37 @@ interface ILangSelector {
   lang: string;
 }
 
-const langData: ILangSelector[] = [{ lang: 'UA' }, { lang: 'FR' }, { lang: 'DE ' }];
+const langData: ILangSelector[] = [{ lang: 'EN' }, { lang: 'UA' }, { lang: 'FR' }, { lang: 'DE ' }];
 
 const LangSelector = () => {
   const [langue, setLangue] = useState<string>('EN');
+  const [langShow, setLangShow] = useState(false);
 
   const changeLang = (item: string) => {
     setLangue(item);
+    setLangShow(false);
+  };
+
+  const handleShowMenu = () => {
+    setLangShow(!langShow);
   };
 
   return (
     <div className='lang-selector'>
-      <div className='lang-selector-title'>{langue}</div>
-
-      <ul className='lang-selector-menu'>
-        {langData.map((item) => (
-          <li>
-            <button onClick={() => changeLang(item.lang)}>{item.lang}</button>
-          </li>
-        ))}
-      </ul>
+      <button onClick={() => handleShowMenu()} className='lang-selector-item'>
+        {langue}
+      </button>
+      {langShow && (
+        <ul className='lang-selector-menu '>
+          {langData.map((item) => (
+            <li key={item.lang}>
+              <button className='lang-selector-item' onClick={() => changeLang(item.lang)}>
+                {item.lang}
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
