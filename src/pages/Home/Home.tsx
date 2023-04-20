4e4c6be37b4beb091265540f12947547d19e0cae
dotkurka <moizeromg@gmail.com>
2023-04-21 00:37:55 +0300

    feat: add Article

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,33 +1,16 @@
-import ArticleCollection from 'components/ArticleCollection/ArticleCollection';
+import Article from 'components/Article/Article';
 
-const data = [
-  {
-    img: 'https://images.unsplash.com/photo-1461897104016-0b3b00cc81ee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80',
-    alt: '',
-    title: 'Raiders release',
-    description: 'After a five-year stint with the ',
-    path: '/blalalalal',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1461897104016-0b3b00cc81ee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80',
-    alt: '',
-    title: 'Raiders release',
-    description: 'After a five-year stint with the Oakland Raiders',
-    path: '/blalalalal',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1461897104016-0b3b00cc81ee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80',
-    alt: '',
-    title: 'Raiders release',
-    description: 'After a five-year stint with the Oakland Raiders',
-    path: '/blalalalal',
-  },
-];
-
+const data = {
+  img: 'https://images.unsplash.com/photo-1461897104016-0b3b00cc81ee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80',
+  alt: '',
+  category: 'NBA',
+  description: 'Lorem ipsum dolor sit amet, consectetur',
+  path: '/blalalalal',
+};
 const Home = () => {
   return (
     <div>
-      <ArticleCollection collection={data} />
+      <Article articleData={data} />
     </div>
   );
 };
